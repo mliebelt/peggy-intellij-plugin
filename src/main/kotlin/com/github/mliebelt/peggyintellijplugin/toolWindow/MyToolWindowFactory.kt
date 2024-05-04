7b41bdf48115dfff2f2cee4d4e994c204edcef07
GitHub Action <action@github.com>
2024-05-04 11:46:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/mliebelt/peggyintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mliebelt/peggyintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mliebelt.peggyintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mliebelt.peggyintellijplugin.MyBundle
+import com.github.mliebelt.peggyintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
